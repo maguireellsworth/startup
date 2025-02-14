4d0fa331d1ff5047547ad9ad01770a6b7f589836
Maguire Ellsworth <m3k4e@byu.edu>
2025-02-13 21:56:31 -0700

    mend

--- src/app.jsx
+++ src/app.jsx
@@ -1,36 +1,56 @@
 import React from 'react';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Home } from './home/home';
+import { Post } from './post/post';
+import { About } from './about/about';
 
 export default function App(){
     return(
-        <div>
-            <header>
-                <h1>KeepMePosted</h1>
-                <nav>
-                    <menu>
-                        <ul class="header-menu">
-                            <li><a href="index.html" class="header-link">Login</a></li>
-                            <li><a href="home.html" class="header-link">Home</a></li>
-                            <li><a href="post.html" class="header-link">Post</a></li>
-                            <li><a href="about.html" class="header-link">About</a></li>
-                        </ul>
-                    </menu>
-                </nav>
-            </header>
-            <footer>
-                <div class="smudge-cat">
-                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><img src="./Photos/Peeker_Cat.png" class="smudge_cat"/></a>
-                </div>
-                <div id="contact">
-                    <h2>Maguire Ellsworth</h2>
-                    <h4>Arch Wizard and Caretaker of KeepMePosted </h4>
-                    <p>See behind the screen here -----{'>'} <a href="https://github.com/maguireellsworth/startup">Github</a></p>
-                </div>
-                <div id="weather">
-                    <p>Super Sunny Today!</p>
-                    <p>78℉ 🌞😎</p>
-                </div>
-            </footer>
+        <div className="app-container">
+            <BrowserRouter>
+                <header className="header">
+                    <h1>KeepMePosted</h1>
+                    <nav>
+                        <menu>
+                            <ul className="header-menu">
+                                <li><NavLink to="" className="header-link">Login</NavLink></li>
+                                <li><NavLink to="home" className="header-link">Home</NavLink></li>
+                                <li><NavLink to="post" className="header-link">Post</NavLink></li>
+                                <li><NavLink to="about" className="header-link">About</NavLink></li>
+                            </ul>
+                        </menu>
+                    </nav>
+                </header>
+
+                <Routes>
+                    <Route path="/" element={<Login />} exact />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/post" element={<Post />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+
+                <footer>
+                    <div className="smudge-cat">
+                        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><img src="/Peeker_Cat.png" className="smudge_cat"/></a>
+                    </div>
+                    <div id="contact">
+                        <h2>Maguire Ellsworth</h2>
+                        <h4>Arch Wizard and Caretaker of KeepMePosted </h4>
+                        <p>See behind the screen here -----{'>'} <a href="https://github.com/maguireellsworth/startup">Github</a></p>
+                    </div>
+                    <div id="weather">
+                        <p>Super Sunny Today!</p>
+                        <p>78℉ 🌞😎</p>
+                    </div>
+                </footer>
+        </BrowserRouter>
         </div>
     )
+}
+
+function NotFound(){
+    return <main>404: Return to sender. Address unknown.</main>
 }
