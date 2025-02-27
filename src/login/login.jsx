4af2f7a42b8ffb961a316bfebddc8de960e04038
Maguire Ellsworth <m3k4e@byu.edu>
2025-02-27 13:20:25 -0700

    started react, no visual

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import './login.css';
 
-export function Login(){
+export function Login(setUsers, users){
+    const [user, setUser] = React.useState(localStorage.getItem("user") || null);
+    const [bool, setBool] = false;
+    const [username, setUsername] = null;
+    const [password, setPassword] = null;
+
+    function loginUser(){
+
+    }
+
+    function registerUser(){
+
+    }
+
+    function userameChange(e){
+        setUsername(e.target.value);
+    }
+
+
     return(
             <main className='login'>
                 <h2>Welcome!</h2>
                 <h3>Ready to see whats happening?</h3>
                 <div className="sign-in-container">
-                    <form method="get" action="home.html" className="sign-in">
+                    <form method="get" className="sign-in">
                         <p>Username:</p>
                         <input type="text" placeholder="xXEpicGamerXx"/>
                         <p>Password:</p>
-                        <input type="password" placeholder="password"/>
-                        <button type="submit">Login</button>
-                        <button type="submit">Sign-Up</button>
+                        <input type="password" placeholder="password" onChange={usernameChange}/>
+                        <button type="submit" onClick={loginUser}>Login</button>
+                        {bool && <p>User already exists!</p>}
+                        <button type="submit" onClick={registerUser}>Sign-Up</button>
                     </form>
                 </div>
             </main>
