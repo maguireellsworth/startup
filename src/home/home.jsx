706489447864e99f0440dce9c0098db9a322c317
Maguire Ellsworth <m3k4e@byu.edu>
2025-02-28 14:24:36 -0700

    implemented websocket emulation

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -3,6 +3,47 @@ import './home.css';
 
 export function Home({user}){
     const [posts, setPosts] = React.useState(JSON.parse(localStorage.getItem("posts")) || []);
+    const [counter, setCounter] = React.useState(1);
+    const fakePosts = [
+        {
+            title: "This is a title",
+            content:" This is some content",
+            username: "IRock"
+        },
+        {
+            title: "This is another title",
+            content:" This is some more content",
+            username: "yourMom"
+        },
+        {
+            title: "placeHolder",
+            content: "Have some content",
+            username: "DwaynJohnson"
+        },
+        {
+            title: "Another one",
+            content: "Youre not even reading these are you?",
+            username: "TheRiddler"
+        },
+        {
+            title: "This should be plenty",
+            content: "But we might add one more, you might even say... another one",
+            username: "someRapper"
+        }
+    ]
+
+    React.useEffect(()=>{
+        const interval = setInterval(()=>{
+            const newPost = fakePosts[Math.floor(Math.random() * fakePosts.length)];
+            posts.push(JSON.stringify(newPost))
+            setPosts(posts);
+            localStorage.setItem("posts", JSON.stringify(posts));
+            setCounter(counter + 1);
+        }, 5000)
+
+        return ()=> clearInterval(interval);
+    }, [counter]);
+
     return(
         <main className="home">
             <h3 className="userWelcome">{user}'s feed</h3>
@@ -13,7 +54,7 @@ export function Home({user}){
                         <div className="post">
                         <h2 className="title">{post.title}</h2>
                         <p className="content">{post.content}</p>
-                        {/* <img src="/witch_brew.jpg" className="post-image"/> */}
+                        <img src={post.image} className="post-image"/>
                         <h4 className="user">{post.username}</h4>
                     </div>
                     )
