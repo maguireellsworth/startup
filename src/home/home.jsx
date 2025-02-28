78c95ebf309ff54f98a19c190aa6005de3db14c1
Maguire Ellsworth <m3k4e@byu.edu>
2025-02-28 14:49:33 -0700

    updated fakePosts in home

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -7,12 +7,12 @@ export function Home({user}){
     const fakePosts = [
         {
             title: "This is a title",
-            content:" This is some content",
+            content: "Why do melons have weddings? They cantelope.",
             username: "IRock"
         },
         {
-            title: "This is another title",
-            content:" This is some more content",
+            title: "Jokie Joke",
+            content: "Watch what you say around the egg whites. They can't take a yolk.",
             username: "yourMom"
         },
         {
@@ -39,7 +39,8 @@ export function Home({user}){
             setPosts(posts);
             localStorage.setItem("posts", JSON.stringify(posts));
             setCounter(counter + 1);
-        }, 5000)
+        }, 20000)
+        //TODO change back to 5000
 
         return ()=> clearInterval(interval);
     }, [counter]);
@@ -60,80 +61,6 @@ export function Home({user}){
                     )
                 })}
             </div>
-
-            {/* <div className="post-container">
-                <div className="post">
-                    <h2 className="title">New Cauldron!</h2>
-                    <p className="content">Better prepare for the spiciest potions youve never heard of!</p>
-                    <img src="/witch_brew.jpg" className="post-image"/>
-                    <h4 className="user">LocalWartlessWitch</h4>
-                </div>
-                <div className="post">
-                    <h2 className="title">Sword shattered during last raid of ogre den</h2>
-                    <p className="content">Looking for willing blacksmith to commission a new one and talented enchanter to imbue with Smite IV</p>
-                    <h4 className="user">FeyHunter</h4>
-                </div>
-                <div className="post">
-                    <h2 className="title">Funding Goal Reached!</h2>
-                    <p className="content">Our first round of funding has reached its goal and we can now start on production of griffin down jackets!</p>
-                    <img src="/furcoat.jpg" className="post-image" />
-                    <h4 className="user">Layers&Litches</h4>
-                </div>
-                <div className="post">
-                    <h2 className="title">Placeholder_Title</h2>
-                    <p className="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam auctor interdum felis sed tempor. Etiam vitae lacus ac nibh varius luctus in vitae neque. Pellentesque scelerisque ipsum non nibh auctor hendrerit. Donec vitae dui enim. Donec a arcu neque. Etiam sed volutpat eros, eu imperdiet elit. </p>
-                    <h4 className="user">Placeholder_user</h4>
-                </div>
-                <div className="post">
-                    <h2 className="title">Placeholder_Title</h2>
-                    <p className="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam auctor interdum felis sed tempor. Etiam vitae lacus ac nibh varius luctus in vitae neque. Pellentesque scelerisque ipsum non nibh auctor hendrerit. Donec vitae dui enim. Donec a arcu neque. Etiam sed volutpat eros, eu imperdiet elit. </p>
-                    <h4 className="user">Placeholder_user</h4>
-                </div>
-                <div className="post">
-                    <h2 className="title">Placeholder_Title</h2>
-                    <p className="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam auctor interdum felis sed tempor. Etiam vitae lacus ac nibh varius luctus in vitae neque. Pellentesque scelerisque ipsum non nibh auctor hendrerit. Donec vitae dui enim. Donec a arcu neque. Etiam sed volutpat eros, eu imperdiet elit. </p>
-                    <h4 className="user">Placeholder_user</h4>
-                </div>
-                <div className="post">
-                    <h2 className="title">Placeholder_Title</h2>
-                    <p className="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam auctor interdum felis sed tempor. Etiam vitae lacus ac nibh varius luctus in vitae neque. Pellentesque scelerisque ipsum non nibh auctor hendrerit. Donec vitae dui enim. Donec a arcu neque. Etiam sed volutpat eros, eu imperdiet elit. </p>
-                    <h4 className="user">Placeholder_user</h4>
-                </div>
-                <div className="post">
-                    <h2 className="title">Placeholder_Title</h2>
-                    <p className="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam auctor interdum felis sed tempor. Etiam vitae lacus ac nibh varius luctus in vitae neque. Pellentesque scelerisque ipsum non nibh auctor hendrerit. Donec vitae dui enim. Donec a arcu neque. Etiam sed volutpat eros, eu imperdiet elit. </p>
-                    <h4 className="user">Placeholder_user</h4>
-                </div>
-                <div className="post">
-                    <h2 className="title">Placeholder_Title</h2>
-                    <p className="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam auctor interdum felis sed tempor. Etiam vitae lacus ac nibh varius luctus in vitae neque. Pellentesque scelerisque ipsum non nibh auctor hendrerit. Donec vitae dui enim. Donec a arcu neque. Etiam sed volutpat eros, eu imperdiet elit. </p>
-                    <h4 className="user">Placeholder_user</h4>
-                </div>
-                <div className="post">
-                    <h2 className="title">Placeholder_Title</h2>
-                    <p className="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam auctor interdum felis sed tempor. Etiam vitae lacus ac nibh varius luctus in vitae neque. Pellentesque scelerisque ipsum non nibh auctor hendrerit. Donec vitae dui enim. Donec a arcu neque. Etiam sed volutpat eros, eu imperdiet elit. </p>
-                    <h4 className="user">Placeholder_user</h4>
-                </div>
-                <div className="post">
-                    <h2 className="title">Placeholder_Title</h2>
-                    <p className="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam auctor interdum felis sed tempor. Etiam vitae lacus ac nibh varius luctus in vitae neque. Pellentesque scelerisque ipsum non nibh auctor hendrerit. Donec vitae dui enim. Donec a arcu neque. Etiam sed volutpat eros, eu imperdiet elit. </p>
-                    <h4 className="user">Placeholder_user</h4>
-                </div>
-                <div className="post">
-                    <h2 className="title">Placeholder_Title</h2>
-                    <p className="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam auctor interdum felis sed tempor. Etiam vitae lacus ac nibh varius luctus in vitae neque. Pellentesque scelerisque ipsum non nibh auctor hendrerit. Donec vitae dui enim. Donec a arcu neque. Etiam sed volutpat eros, eu imperdiet elit. </p>
-                    <h4 className="user">Placeholder_user</h4>
-                </div>
-                <div className="post">
-                    <h2 className="title">Placeholder_Title</h2>
-                    <p className="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam auctor interdum felis sed tempor. Etiam vitae lacus ac nibh varius luctus in vitae neque. Pellentesque scelerisque ipsum non nibh auctor hendrerit. Donec vitae dui enim. Donec a arcu neque. Etiam sed volutpat eros, eu imperdiet elit. </p>
-                    <h4 className="user">Placeholder_user</h4>
-                </div>
-                <div className="post">
-                    <h2 className="title">Placeholder_Title</h2>
-                    <p className="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam auctor interdum felis sed tempor. Etiam vitae lacus ac nibh varius luctus in vitae neque. Pellentesque scelerisque ipsum non nibh auctor hendrerit. Donec vitae dui enim. Donec a arcu neque. Etiam sed volutpat eros, eu imperdiet elit. </p>
-                    <h4 className="user">Placeholder_user</h4>
-                </div> */}
         </main>
     )
 }
